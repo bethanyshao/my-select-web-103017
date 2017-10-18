04edb617b11028a0ba8d751239de85795af82b40
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-18 18:29:00 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ new_array = []
+ i = 0
+ while i < array.length do
+   if yield(array[i])
+     new_array.push(yield(array[i]))
+   end
+ end
+ new_array
 end
