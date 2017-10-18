6b97498fd0aafb72dc495f31a99a27cd2ddbf2a9
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-18 18:59:12 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(array)
   new_array = []
   i = 0
   while i < array.length do
-    
+    if yield(array[i]) == true
+      new_array.push(array[i])
+    end
     i += 1
   end
   new_array
